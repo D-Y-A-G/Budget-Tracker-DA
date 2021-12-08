fb7a944691dc65a70aa4b5ffc68b9169b60d3a35
Denis Arce <dnsarce21@gmail.com>
2021-12-07 19:58:11 -0600

    linked manifest on html and updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,12 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/indexedDb.js",
-  "/public/styles.css",
+  "/styles.css",
   "/manifest.webmanifest",
-  "/dist/favorites.bundle.js",
-  "/dist/topic.bundle.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
+  "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
 ];
 
 const PRECACHE = "precache-v1";
