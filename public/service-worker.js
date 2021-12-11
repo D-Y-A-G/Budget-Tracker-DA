23334274ce37e2d142de89d367bf61831456a31b
Denis Arce <dnsarce21@gmail.com>
2021-12-11 14:22:24 -0600

    updated sw code for heroku deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,6 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/indexedDb.js",
-  "../models/transaction.js",
   "/styles.css",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
